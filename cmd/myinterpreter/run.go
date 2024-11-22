2845f0c857b892fcf7df12268ceb58dac51bcdb3
LitoleNINJA <74725722+LitoleNINJA@users.noreply.github.com>
2024-11-22 14:49:45 +0000

    initial commit

--- cmd/myinterpreter/run.go
+++ cmd/myinterpreter/run.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"unicode"
 )
 
 var values map[string]string
+var lines [][]byte
+var lineNumber int
+
+func readLines(fileContent []byte) [][]byte {
+	var lines [][]byte
+	line := make([]byte, 0)
+	for i := 0; i <= len(fileContent); i++ {
+		if i == len(fileContent) || fileContent[i] == 10 {
+			line = []byte(strings.TrimSpace(string(line)))
+			// fmt.Printf("Line : %s\n", line)
+			lines = append(lines, line)
+			line = make([]byte, 0)
+		} else {
+			line = append(line, fileContent[i])
+		}
+	}
+
+	return lines
+}
 
 func getPrintContents(line []byte) []byte {
 	s, ok := strings.CutPrefix(string(line), "print")
@@ -26,24 +44,6 @@ func getPrintContents(line []byte) []byte {
 func isPrintStmt(stmt []byte) bool {
 	stmtString := string(stmt)
 	return strings.HasPrefix(stmtString, "print")
-}
-
-func readLines(fileContent []byte) [][]byte {
-	var lines [][]byte
-	line := make([]byte, 0)
-	for i := 0; i < len(fileContent); i++ {
-		if fileContent[i] == 59 {
-			line = []byte(strings.TrimSpace(string(line)))
-			// fmt.Printf("Line : %s\n", line)
-			lines = append(lines, line)
-			line = make([]byte, 0)
-		} else {
-			line = append(line, fileContent[i])
-
-		}
-	}
-
-	return lines
 }
 
 func isVarDeclaration(stmt []byte) bool {
@@ -75,41 +75,81 @@ func getVarDeclaration(stmt []byte) error {
 	return nil
 }
 
+func isBlockStart(stmt []byte) bool {
+	stmtString := strings.TrimSpace(string(stmt))
+
+	return stmtString == "{"
+}
+
+func isBlockEnd(stmt []byte) bool {
+	stmtString := strings.TrimSpace(string(stmt))
+
+	return stmtString == "}"
+}
+
 func run(fileContents []byte) error {
-	lines := readLines(fileContents)
+	lineNumber = 0
+	lines = readLines(fileContents)
 	values = make(map[string]string)
 	// fmt.Println(lines)
-	for _, stmt := range lines {
-		printStmt := false
-		if isPrintStmt(stmt) {
-			printStmt = true
-			stmt = getPrintContents(stmt)
-		} else if isVarDeclaration(stmt) {
-			err := getVarDeclaration(stmt)
-			if err != nil {
-				return err
-			}
-			continue
+	for {
+		if lineNumber >= len(lines) {
+			break
 		}
 
-		if strings.Contains(string(stmt), "=") {
-			handleAssignment(string(stmt))
-		}
+		stmt := lines[lineNumber]
 
-		if len(stmt) == 0 {
-			os.Exit(65)
-		}
-
-		// fmt.Printf("Eval : %s, Len : %d\n", stmt, len(stmt))
-		expr, err := evaluate(stmt)
+		err := handleStmt(stmt)
 		if err != nil {
 			return err
 		}
 
-		if printStmt {
-			fmt.Println(expr)
-		}
+		lineNumber++
 	}
+	return nil
+}
+
+func handleStmt(stmt []byte) error {
+	printStmt := false
+
+	if isPrintStmt(stmt) {
+		printStmt = true
+		stmt = getPrintContents(stmt)
+	} else if isVarDeclaration(stmt) {
+		err := getVarDeclaration(stmt)
+		if err != nil {
+			return err
+		}
+		return nil
+	} else if isBlockStart(stmt) {
+		fmt.Printf("Start Pos : %d\n", lineNumber)
+		err := handleBlock()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("End Pos : %d\n", lineNumber)
+		return nil
+	}
+
+	if strings.Contains(string(stmt), "=") {
+		handleAssignment(string(stmt))
+	}
+
+	if len(stmt) == 0 {
+		exitCode = 65
+		return fmt.Errorf("empty stmt")
+	}
+
+	fmt.Printf("Eval : %s, Len : %d\n", stmt, len(stmt))
+	expr, err := evaluate(stmt)
+	if err != nil {
+		return err
+	}
+
+	if printStmt {
+		fmt.Println(expr)
+	}
+
 	return nil
 }
 
@@ -143,4 +183,27 @@ func handleAssignment(stmt string) (string, error) {
 
 		return val, nil
 	}
+}
+
+func handleBlock() error {
+	// localValues := make(map[string]string)
+	for {
+		lineNumber++
+		if lineNumber >= len(lines) {
+			break
+		}
+
+		stmt := lines[lineNumber]
+
+		if isBlockEnd(stmt) {
+			return nil
+		}
+
+		err := handleStmt(stmt)
+		if err != nil {
+			return err
+		}
+	}
+
+	return fmt.Errorf("Error at end: Expect '}' .")
 }
